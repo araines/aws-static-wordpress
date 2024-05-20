66bed6af7cdd193ac04fc598f93606e25ec8fc3a
Andy Raines <andrew.raines@news.co.uk>
2024-05-20 08:36:36 +0100

    feat: initial commit

---
+++ vpc.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "${var.site_domain}-vpc"
+  cidr = "10.0.0.0/16"
+
+  azs            = ["${var.aws_region}a", "${var.aws_region}b"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
+}
